3c58e049fd665cc639c3045f27aca6cefb92c315
sachinsharma1001 <sachin.sharma100197@gmail.com>
2020-12-15 13:02:28 +0530

    Add serverless project files

---
+++ src/lambda/http/createImages.ts
@@ -0,0 +1,59 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
+import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const groupsTable = process.env.GROUPS_TABLE;
+const imagesTable = process.env.IMAGES_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log("Create Image:" + event);
+
+    const groupId = event.pathParameters.groupId;
+
+    const validGroupId = await groupExists(groupId);
+    if(!validGroupId) {
+        return {
+            statusCode: 404,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                error: 'Group Id does not exists...'
+            })
+        }
+    }
+
+    const parsedBody = JSON.parse(event.body);
+    const itemId = uuid.v4();
+    const newItem = {
+        id: itemId,
+        ...parsedBody
+    }
+
+    await docClient.put({
+        TableName: imagesTable,
+        Item: newItem
+    }).promise()
+
+    return {
+        statusCode: 201,
+        headers: {
+        'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            newItem
+        })
+    }
+}
+
+async function groupExists(groupId: string) {
+    const result = await docClient.get({
+        TableName: groupsTable,
+        Key: {
+            id: groupId
+        }
+    }).promise()
+
+    return !!result.Item;
+}
